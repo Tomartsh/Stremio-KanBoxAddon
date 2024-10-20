0c650ab4fbd9a55a069956af8311cec402127096
Tomartsh <61180368+Tomartsh@users.noreply.github.com>
2024-10-20 10:52:36 +0300

    added image prefix to state the server location of the images

--- classes/constants.js
+++ classes/constants.js
@@ -2,5 +2,6 @@ module.exports = {
     url_kanbox: "https://www.kan.org.il/lobby/kan-box/",
     url_live: "https://www.kan.org.il/live/",
     prefix_kanbox: "kanbox_",
-    debugState: "true"
+    debugState: "true",
+    image_prefix: "https://www.kan.org.il"
 };
